644df4cc62614e964800c0a70c30fa26adbf9f77
stephentandjiria <75295743+stephentandjiria@users.noreply.github.com>
2021-09-14 15:38:34 +0700

    postgres

--- app.py
+++ app.py
@@ -8,10 +8,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
+import os
+
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# app.config['SECRET_KEY'] = '9A1945'
+app.config['SECRET_KEY'] = os.getenv("9A1945")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
