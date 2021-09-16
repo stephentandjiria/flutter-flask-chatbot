52eab01189a6f8cee469627639b938be00bdec20
stephentandjiria <75295743+stephentandjiria@users.noreply.github.com>
2021-09-16 13:03:52 +0700

    get_user API (fixed to online DB)

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ import time
 app = Flask(__name__)
 
 # Offline Mode
-app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Online Mode (Postgre)
-# app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -40,7 +40,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(1000))
 
 #Line below only required once, when creating DB.
-db.create_all()
+# db.create_all()
 
 @app.route('/')
 def home():
